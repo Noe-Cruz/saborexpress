50b319a15b00d83156bac06a83e697d524e943fd
Noe-Cruz <jn37noe@gmail.com>
2024-11-08 22:22:49 -0600

    versión 3.1

--- src/index.js
+++ src/index.js
@@ -16,20 +16,35 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+//serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onSuccess: () => {
+    // Register Firebase messaging service worker once the main worker is ready
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/firebase-messaging-sw.js')
+        .then((registration) => {
+          console.log('Firebase messaging service worker registered.');
+        })
+        .catch((error) => {
+          console.error('Failed to register Firebase service worker:', error);
+        });
+    }
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker de Firebase registrado');
-    })
-    .catch((error) => {
-      console.error('Error al registrar el Service Worker de Firebase: ', error);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/firebase-messaging-sw.js')
+//     .then((registration) => {
+//       console.log('Service Worker de Firebase registrado');
+//     })
+//     .catch((error) => {
+//       console.error('Error al registrar el Service Worker de Firebase: ', error);
+//     });
+// }
